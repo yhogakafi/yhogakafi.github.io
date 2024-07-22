755e5c87885bf673d74f21beb9cc3647e389ec24
yhogakafi <yhogadoang3@gmail.com>
2024-07-22 17:07:10 +0700

    update toggle margin

--- theme.js
+++ theme.js
@@ -1,17 +1,6 @@
-// document.getElementById('dark-mode-toggle').addEventListener('change', (event) => {
-//     const linkElement = document.getElementById('theme-stylesheet');
-//     if (event.target.checked) {
-//         linkElement.setAttribute('href', 'dark-mode.css');
-//     } else {
-//         linkElement.setAttribute('href', 'light-mode.css');
-//     }
-// });
-
-document.querySelector('.toggle-state').addEventListener('change', () => {
+document.getElementById('dark-mode-toggle').addEventListener('change', (event) => {
     const linkElement = document.getElementById('theme-stylesheet');
-    const currentTheme = linkElement.getAttribute('href');
-
-    if (currentTheme === 'light-mode.css') {
+    if (event.target.checked) {
         linkElement.setAttribute('href', 'dark-mode.css');
     } else {
         linkElement.setAttribute('href', 'light-mode.css');
