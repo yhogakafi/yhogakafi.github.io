40f0c78bfae206fbb28486edcb34845ac613b4e5
yhogakafi <yhogadoang3@gmail.com>
2024-07-22 16:13:53 +0700

    update toggle

--- theme.js
+++ theme.js
@@ -1,6 +1,17 @@
-document.getElementById('dark-mode-toggle').addEventListener('change', (event) => {
+// document.getElementById('dark-mode-toggle').addEventListener('change', (event) => {
+//     const linkElement = document.getElementById('theme-stylesheet');
+//     if (event.target.checked) {
+//         linkElement.setAttribute('href', 'dark-mode.css');
+//     } else {
+//         linkElement.setAttribute('href', 'light-mode.css');
+//     }
+// });
+
+document.querySelector('.toggle-state').addEventListener('change', () => {
     const linkElement = document.getElementById('theme-stylesheet');
-    if (event.target.checked) {
+    const currentTheme = linkElement.getAttribute('href');
+
+    if (currentTheme === 'light-mode.css') {
         linkElement.setAttribute('href', 'dark-mode.css');
     } else {
         linkElement.setAttribute('href', 'light-mode.css');
