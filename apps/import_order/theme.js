631d7d36b56c16cc3cd53a0087ee03bd0e785cdb
yhogakafi <yhogadoang3@gmail.com>
2024-07-29 09:53:51 +0700

    Update theme.js

--- apps/import_order/theme.js
+++ apps/import_order/theme.js
@@ -1,17 +1,16 @@
+// Toggle dark and light modes
 document.getElementById('dark-mode-toggle').addEventListener('change', (event) => {
     const linkElement = document.getElementById('theme-stylesheet');
-    if (event.target.checked) {
-        linkElement.setAttribute('href', 'dark-mode.css');
-    } else {
-        linkElement.setAttribute('href', 'light-mode.css');
-    }
+    const theme = event.target.checked ? 'dark-mode.css' : 'light-mode.css';
+    linkElement.setAttribute('href', theme);
 });
 
+// Validate file input for file1
 document.getElementById('file1').addEventListener('change', function(event) {
     const files = event.target.files;
-    const validSubstring = 'template.xlsx'.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+    const validSubstring = 'template.xlsx'.toLowerCase(); // Expected file name substring
     for (let i = 0; i < files.length; i++) {
-        const fileName = files[i].name.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+        const fileName = files[i].name.toLowerCase();
         if (!fileName.includes(validSubstring)) {
             alert('Hanya bisa memilih file dengan nama template.xlsx');
             event.target.value = ''; // Clear the input
@@ -20,13 +19,14 @@ document.getElementById('file1').addEventListener('change', function(event) {
     }
 });
 
+// Validate file input for file3
 document.getElementById('file3').addEventListener('change', function(event) {
     const files = event.target.files;
-    const validSubstring = '_no_order.xlsx'.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+    const validSubstring = '_no_order.xlsx'.toLowerCase(); // Expected file name substring
     for (let i = 0; i < files.length; i++) {
-        const fileName = files[i].name.toLowerCase(); // Convert to lowercase for case-insensitive comparison
+        const fileName = files[i].name.toLowerCase();
         if (!fileName.includes(validSubstring)) {
-            alert('Hanya bisa memilih file hasil ekspor PDF LABEL PENGIRIMAN dengan nama **_no_order.xlsx');
+            alert('Hanya bisa memilih file hasil ekspor PDF LABEL PENGIRIMAN dengan nama _no_order.xlsx');
             event.target.value = ''; // Clear the input
             break;
         }
